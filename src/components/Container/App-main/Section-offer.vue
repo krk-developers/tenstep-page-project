f2f9d34b8292b120df26b9903e6527fbbad1cfbf
Kamil Sztefko <sztefkokamil@gmail.com>
2019-03-07 20:19:52 +0100

    section-offer - basic structure & styles ADD

--- src/components/Container/App-main/Section-offer.vue
+++ src/components/Container/App-main/Section-offer.vue
@@ -1,6 +1,14 @@
 <template>
   <section class="section-offer">
-offer
+    <div class="top-bar"></div>
+    <div class="offer-header section-header">
+      <h2>Oferta</h2>
+      <h6>Wkrocz z nami w lepszy biznes.</h6>
+    </div>
+    <div class="offer-text">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus viverra laoreet viverra. Maecenas dignissim dui nunc, quis pharetra ligula tempus sed. Donec ullamcorper nibh et pellentesque ultricies. Nulla nec vulputate nulla. Phasellus luctus enim in sodales feugiat. Curabitur molestie orci id nulla tempus elementum. Etiam lectus sapien, blandit et dictum sit amet, aliquam at orci. Vestibulum auctor quis justo ac posuere. Maecenas nec urna justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut lectus ex, suscipit vitae dui ut, iaculis dignissim nisl. Fusce euismod volutpat nibh nec eleifend. Donec sit amet elit finibus, scelerisque nulla et, aliquet mauris. Praesent id risus tortor. Nam interdum rhoncus mi, id rhoncus nulla tempor sit amet. Aliquam sapien nunc, porttitor quis maximus in, rhoncus sed tortor.</p>
+    </div>
+    <div class="offer-content"></div>
   </section>
 </template>
 
@@ -12,7 +20,21 @@ export default {
 
 <style lang="scss">
 .section-offer{
-  height: 400px;
   background: lightgray;
 }
+
+.offer-text{
+  padding:20px;
+
+  p{
+    column-count: 2;
+  }
+}
+
+.offer-content{
+  height: 500px;
+  background: hsl(30, 80%, 60%);
+  margin-bottom: 30px;
+}
+
 </style>
--- src/components/Container/App-main/Section-welcome.vue
+++ src/components/Container/App-main/Section-welcome.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="section-welcome">
     <div class="top-bar"></div>
-    <div class="welcome-header">
+    <div class="welcome-header section-header">
       <h2>Witamy w TenStep</h2>
       <h6>Sprawdź co możemy dla ciebie zrobić.</h6>
     </div>
-    <div class="welcome-body">
+    <div class="welcome-text">
       <p><span class="bold">Lorem ipsum</span> dolor sit amet, consectetur adipiscing elit. Phasellus viverra laoreet viverra. Maecenas dignissim dui nunc, quis pharetra ligula tempus sed. Donec ullamcorper nibh et pellentesque ultricies. Nulla nec vulputate nulla. Phasellus luctus enim in sodales feugiat. Curabitur molestie orci id nulla tempus elementum.</p>
       <p>Etiam lectus sapien, blandit et dictum sit amet, aliquam at orci. Vestibulum auctor quis justo ac posuere. Maecenas nec urna justo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut lectus ex, suscipit vitae dui ut, iaculis dignissim nisl. Fusce euismod volutpat nibh nec eleifend. Donec sit amet elit finibus, scelerisque nulla et, aliquet mauris. Praesent id risus tortor. Nam interdum rhoncus mi, id rhoncus nulla tempor sit amet. Aliquam sapien nunc, porttitor quis maximus in, rhoncus sed tortor.</p>
     </div>
@@ -23,26 +23,7 @@ export default {
   padding-bottom: 30px;
 }
 
-.top-bar{
-  height: 15px;
-  border-bottom-left-radius: 10px;
-  border-bottom-right-radius: 10px;
-  background: hsl(230, 80%, 40%);
-}
-
-.welcome-header{
-  padding: 20px;
-
-  h2{
-    margin: 0;
-  }
-
-  h6{
-    margin: 0;
-  }
-}
-
-.welcome-body{
+.welcome-text{
   display: flex;
   padding: 15px;
 
