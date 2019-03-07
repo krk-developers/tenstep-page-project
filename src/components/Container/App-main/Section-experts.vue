229be144d574b7f3b09de522e276a931e97ea261
Kamil Sztefko <sztefkokamil@gmail.com>
2019-03-07 20:19:33 +0100

    section-experts - basic structure & styles ADD

--- src/components/Container/App-main/Section-experts.vue
+++ src/components/Container/App-main/Section-experts.vue
@@ -1,6 +1,11 @@
 <template>
   <section class="section-experts">
-experts
+    <div class="top-bar"></div>
+    <div class="experts-header section-header">
+      <h2>Eksperci</h2>
+      <h6>Zobacz kto jest w naszym teamie.</h6>
+    </div>
+    <div class="experts-content"></div>
   </section>
 </template>
 
@@ -12,7 +17,13 @@ export default {
 
 <style lang="scss">
 .section-experts{
-  height: 400px;
   background: lightgray;
+  padding-bottom: 30px;
 }
+
+.experts-content{
+  height: 500px;
+  background: hsl(170, 80%, 30%);
+}
+
 </style>
