ec2b547bcbe7afcdd9f018c03be8f21ed228f422
Kamil Sztefko <sztefkokamil@gmail.com>
2019-03-07 22:38:18 +0100

    contact form & info basic styles ADD

---
+++ src/components/Container/App-main/Section-contact-form.vue
@@ -0,0 +1,71 @@
+<template>
+  <form class="contact-form">
+    <h5>Formularz kontaktowy</h5>
+    <label class="label" for="">Imię i nazwisko</label>
+    <input class="input" type="text" id="name">
+    <label class="label" for="">E-mail</label>
+    <input class="input" type="text" id="email">
+    <label class="label" for="">Telefon</label>
+    <input class="input" type="text" id="phone">
+    <label class="label" for="">Treść wiadomości</label>
+    <textarea class="textarea" id="message"></textarea>
+    <button class="button">Wyślij</button>
+  </form>
+</template>
+
+<style lang="scss">
+
+.contact-form{
+  width: 360px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: #fff;
+  border: 1px solid lightgray;
+  box-shadow: 0 0 12px 1px gray;
+  padding: 20px 0 50px 0;
+  position: absolute;
+  top: -50px;
+  left: 200px;
+
+  h5{
+    font-size: 20px;
+    margin: 0;
+  }
+
+  .label{
+    width: 240px;
+    margin-top: 15px;
+    font-size: 14px;
+  }
+
+  .input{
+    width: 240px;
+    height: 36px;
+    border: 1px solid gray;
+  }
+
+  .textarea{
+    width: 240px;
+    height: 160px;
+    border: 1px solid gray;
+  }
+
+  .button{
+    width: 240px;
+    height: 30px;
+    border: 1px solid gray;
+    background: #fff;
+    margin-top: 20px;
+    color: gray;
+    font-weight: 700;
+    font-size: 16px;
+  }
+  .button:hover{
+    background: gray;
+    color: #fff;
+  }
+
+}
+
+</style>
--- src/components/Container/App-main/Section-contact.vue
+++ src/components/Container/App-main/Section-contact.vue
@@ -5,24 +5,90 @@
       <h2>Kontakt</h2>
       <h6>Napisz do nas</h6>
     </div>
-    <div class="contact-content"></div>
+    <div class="contact-content">
+      <contact-form></contact-form>
+      <div class="contact-info">
+        <h3>
+          <span>Jeśli masz jakieś pytania, napisz</span>
+          <span>Chętnie rozwiejemy Twoje wątpliwości.</span>
+        </h3>
+        <div class="contact-person">
+          <div class="person-img"></div>
+          <div class="person-info">
+            <h5>Katarzyna Renn</h5>
+            <p>Dyrektor operacyjny, trenerka, konsultantka. Jej "oczko w głowie" to dokumentacja i poprawność formalna realizowanych projektów. Zajmuje się też relacjami i rozliczeniami z klientami.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import contactForm from './Section-contact-form.vue';
+
 export default {
-  
+  components:{
+    'contact-form': contactForm
+  }
 }
 </script>
 
 <style lang="scss">
 .section-contact{
-  padding-bottom: 30px;
+  padding-bottom: 180px;
 }
 
 .contact-content{
   height: 400px;
-  background: hsl(60, 50%, 30%);
+  background: hsl(230, 60%, 53%);
+  margin-top: 100px;
+  position: relative;
+  display: flex;
+  flex-direction: row-reverse;
+
+  .contact-info{
+    width: 550px;
+    height: 400px;
+    margin-right: 200px;
+
+    h3{
+      margin: 80px 0 50px 0;
+      
+      span{
+        display: block;
+        color: #fff;
+      }
+    }
+
+    .contact-person{
+      background: #fff;
+      display: flex;
+      padding: 5px;
+
+      .person-img{
+        width: 100px;
+        height: 100px;
+        background: red;
+        border-radius: 50%;
+      }
+
+      .person-info{
+
+        margin-left: 20px;
+
+        h5{
+          font-size: 16px;
+        }
+
+        p{
+          width: 400px;
+          margin-top: 20px;
+          font-size: 14px;
+        }
+      }
+    }
+  }
 }
 
 </style>
