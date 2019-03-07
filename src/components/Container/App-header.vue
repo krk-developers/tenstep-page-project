9ff7d66fce5449e8aa1440d8d31503fb609b815f
Kamil Sztefko <sztefkokamil@gmail.com>
2019-03-07 20:19:11 +0100

    app-header - basic structure & styles ADD

--- src/components/Container/App-header.vue
+++ src/components/Container/App-header.vue
@@ -1,6 +1,10 @@
 <template>
   <header>
-header
+    <div class="header-title">
+      <h1>Klinika Biznesu</h1>
+      <h3>Leczymy dużych - Rozwijamy małych</h3>
+    </div>
+    <div class="header-content"></div>
   </header>
 </template>
 
@@ -15,5 +19,41 @@ header{
   width: 100%;
   height: 600px;
   background: hsl(320, 40%, 70%);
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  padding-bottom: 10px;
 }
+
+.header-title{
+  padding: 20px;
+  grid-column: 2/5;
+  grid-row: 2/4;
+
+
+  h1{
+    display: inline-block;
+    width: 310px;
+    margin: 0 0 4px 0;;
+    border-bottom: 1px solid #000;
+    text-align: center;
+  }
+
+  h3{
+    display: inline-block;
+    width: 310px;
+    margin: 0;
+    border-top: 1px solid #000;
+    text-align: center;
+  }
+}
+
+.header-content{
+  width: 800px;
+  height: 300px;
+  background: hsl(230, 80%, 40%);
+  grid-column: 3/10;
+  grid-row: 4/7;
+}
+
 </style>
